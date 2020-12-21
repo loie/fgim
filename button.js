dbe2a7d9bf189755bb2da49b686681ebdf959012
Silas Vedder <singingfox123@protonmail.com>
2020-12-21 10:51:42 +0100

    fgim website initialized.

---
+++ button.js
@@ -0,0 +1,9 @@
+function w3_open() {
+	document.getElementById("sidebar").style.display = "block";
+	document.getElementById("open_button").style.display = "none";
+}
+
+function w3_close() {
+	document.getElementById("sidebar").style.display = "none";
+	document.getElementById("open_button").style.display = "block";
+}
